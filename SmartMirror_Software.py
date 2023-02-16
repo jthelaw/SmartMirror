e38edfe008ffef9efff340a2f35d0760891e38f0
jthelaw <lawson.k.jordan@gmail.com>
2023-02-16 17:33:07 -0600

    added spotify creds

---
+++ SmartMirror_Software.py
@@ -0,0 +1,28 @@
+import sys
+import requests
+import json
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import QTimer
+
+# Spotify credentials
+SPOTIPY_CLIENT_ID = 'your_client_id'
+SPOTIPY_CLIENT_SECRET = 'your_client_secret'
+SPOTIPY_REDIRECT_URI = 'http://localhost:8000/callback'
+
+
+
+class SmartMirror:
+    def __init__(self):
+        #Initialize spotipy client
+        self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
+                                               client_secret=SPOTIPY_CLIENT_SECRET,
+                                               redirect_uri=SPOTIPY_REDIRECT_URI))
+        # Create the GUI
+        self.app = QApplication(sys.argv)
+        self.window = QWidget()
+        self.window.setWindowTitle('Smart Mirror')
+        self.window.setGeometry(200, 200, 600, 400)
+               
